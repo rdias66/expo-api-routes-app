610f1d2ebe9fa35b695b1a7ae4b1bf7b375b5d85
rdias66 <dias.a@live.com>
2024-04-18 12:01:20 -0300

    login post endpoint

--- src/app/user+api.ts
+++ src/app/user+api.ts
@@ -1,7 +1,25 @@
-import { ExpoResponse } from 'expo-router/server'
+import { ExpoRequest, ExpoResponse } from 'expo-router/server'
 
 export async function GET(): Promise<ExpoResponse> {
   return ExpoResponse.json({
     message: 'Expo router accessed via get request!',
+  })
+}
+
+//Login
+export async function POST(request: ExpoRequest): Promise<ExpoResponse> {
+  const { email, pass } = await request.json()
+
+  if (email === 'test@email.com' && pass === 'dev') {
+    return ExpoResponse.json({
+      email,
+      message: 'Login succesful',
+    })
+  }
+  return new ExpoResponse('Login info incorrect', {
+    status: 404,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
   })
 }
